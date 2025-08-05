43775747b70d34d2bd7d5cd1dd6965d80c9098ef
macpranesh <havocpranesh@gmail.com>
2025-08-05 18:20:30 +0800

    Update Resume Matcher Web App

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Resume-matcher/', // replace with your repo name
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
